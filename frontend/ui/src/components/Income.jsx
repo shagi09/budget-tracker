62dd6c8bc41e5212c7c64b78685e5f3cd3408ddb
shagi09 <shalomwubu024@gmail.com>
2024-06-22 23:17:16 +0300

    states added

--- frontend/ui/src/components/Income.jsx
+++ frontend/ui/src/components/Income.jsx
@@ -1,25 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoIosAdd } from "react-icons/io";
 
 const Income = () => {
+  const [formData,setFormData]=useState({
+    amount:'',
+    date:'',
+    category:'',
+    description:''
+
+  })
+
+  function HandleChange(e){
+    setFormData({
+      ...formData,
+      [e.target.name]:e.target.value
+    })
+
+}
+  const Incomes=[
+    {
+      amount: '2000 birr',
+      date:'22/04/2022',
+      category:'bitcoin',
+      reference:'this is bitcoin income'
+
+    }
+  ]
   return (
     <div className='income-container'>
         <h1>Incomes</h1>
-        <h2>Total Income: $10,000</h2>
+        <h2>Total Income:{formData.amount}$</h2>
         <div className='form-container'> 
           <form className='form' action="">
-            <input type="text" placeholder='Income type' />
-            <input type="number" placeholder='Income Amount' />
-            <input type="date" placeholder='Enter a Date'/>
-            <select name="" id="">
-              <option value="">Salary</option>
-              <option value="">Freelance</option>
-              <option value="">Investments</option>
-              <option value="">Stocks</option>
-              <option value="">Youtube</option>
-              <option value="">Others</option>
+            <input name='amount' type="number" placeholder='Income Amount' value={formData.amount} onChange={HandleChange}/>
+            <input name='date' type="date" placeholder='Enter a Date' value={formData.date} onChange={HandleChange}/>
+            <select name="category" id="" value={formData.category} onChange={HandleChange}>
+              <option value='Salary'>Salary</option>
+              <option value="Freelance">Freelance</option>
+              <option value="Investments">Investments</option>
+              <option value="Stocks">Stocks</option>
+              <option value="Youtube">Youtube</option>
+              <option value="Others">Others</option>
             </select>
-            <input type="text" placeholder='Income Description' />
+            <input name='description' type="text" placeholder='Income Description' value={formData.description} onChange={HandleChange}/>
             <button className='income-btn'><IoIosAdd style={{fontSize: '20px',padding:'2px'}}/> Add Income</button>
           </form>
         </div>
