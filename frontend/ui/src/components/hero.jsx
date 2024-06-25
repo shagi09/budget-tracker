404471afdf45f1530a135546c593fe80fd6cb0cd
shagi09 <shalomwubu024@gmail.com>
2024-06-25 12:48:44 +0300

    styles added

--- frontend/ui/src/components/hero.jsx
+++ frontend/ui/src/components/hero.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import transaction from'../images/transaction.jpg'
+import spending from '../images/spending.jpg'
+import income from '../images/income.jpg'
 
 const Hero = () => {
   return (
@@ -11,6 +14,37 @@ const Hero = () => {
 
              <p className='p-3'>how to improve</p>
         <button className='get-started-btn'>Get Started</button>
+        <div className='transaction-content'>
+          <div className='transaction-image'>
+          <img src={transaction} alt="" />
+          </div>
+          <div className='transaction-header'>
+          <h2>Effortless Transaction Tracking</h2>
+          <p>Discover the ease of viewing all your transactions in one <br />intuitive dashboard. Stay in control of your spending <br />with real-time updates and never miss a single detail.</p>
+          </div>
+          </div>
+          <div className='spending-content'> 
+
+          <div className='spending-header'>
+          <h2>In-Depth Spending Insights</h2>
+          <p>Delve into comprehensive statistics that reveal your <br />spending habits. Our detailed reports empower you to make <br />informed decisions and optimize your financial health.</p>
+          </div>
+          <div className='spending-image'>
+          <img src={spending} alt="" />
+          </div>
+          </div>
+          <div className='income-content'> 
+          <div className='income-image'>
+<img src={income} alt="" />
+</div>
+
+<div className='income-header'>
+<h2>In-Depth Spending Insights</h2>
+<p>Delve into comprehensive statistics that reveal your <br />spending habits. Our detailed reports empower you to make <br />informed decisions and optimize your financial health.</p>
+</div>
+
+</div>
+
       </div>
 
 
