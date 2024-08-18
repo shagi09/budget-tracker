d78fe1613a28e387dc0b05f41232385025e8cd9c
shagi09 <shalomwubu024@gmail.com>
2024-08-18 23:25:25 +0300

    token and cookies created

--- backend/controllers/authcontroller.js
+++ backend/controllers/authcontroller.js
@@ -1,9 +1,21 @@
 const User=require('../models/user')
 const bcrypt=require('bcrypt')
+const jwt=require('jsonwebtoken')
+
+function createToken(id){
+    const secret='shalom secret'
+    const options={expiresIn:'1h'}
+    return jwt.sign({id},secret,options)
+
+}
+
+
 module.exports.SignUp_Post=async(req,res)=>{
     const{email,password}=req.body
     try{
         const user=await User.create({email,password})
+        const token=createToken(user._id)
+        res.cookie('jwt',token,{httpOnly:true})
 
         res.json(user);
 
@@ -25,6 +37,8 @@ module.exports.LogIn_Post=async(req,res)=>{
     if(!isMatch){
        return res.json('incorrect password')
     }
+    const token=createToken(user._id)
+    res.cookie('jwt',token,{httpOnly:true})
         res.json(user)
     
 
