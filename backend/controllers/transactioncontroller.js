fb793204d4b0efcc4e9ba9037beca571fa252949
shagi09 <shalomwubu024@gmail.com>
2024-08-22 10:45:36 +0300

    transactions retrieved by sepecific date

--- backend/controllers/transactioncontroller.js
+++ backend/controllers/transactioncontroller.js
@@ -1,5 +1,27 @@
-const expense=require('../models/expense')
-module.exports.transaction_Get=(req,res)=>{
-    res.json('transactions')
+const Expense=require('../models/expense')
+module.exports.transaction_Get=async (req,res)=>{
+    const { date } = req.body;
 
-}
+    try {
+        // Convert the date from string to a Date object
+        const parsedDate = new Date(date);
+        const startOfDay = new Date(parsedDate.setHours(0, 0, 0, 0));
+        const endOfDay = new Date(parsedDate.setHours(23, 59, 59, 999));
+
+        // Fetch expenses for the specified date
+        const expenses = await Expense.find({
+            date: {
+                $gte: startOfDay,
+                $lte: endOfDay
+            }
+        });
+
+        res.status(200).json({
+            date: date,
+            expenses: expenses
+        });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
